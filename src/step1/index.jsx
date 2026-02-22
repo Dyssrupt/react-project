a6fa90ca4e3463cf0e7f0a8e7490cfb35771f433
Dyssrupt <154683269+Dyssrupt@users.noreply.github.com>
2026-02-22 11:56:33 +0530

    Components created

--- src/step1/index.jsx
+++ src/step1/index.jsx
@@ -3,4 +3,16 @@ import Subtraction from './subtraction';
 import Multiplication from './multiplication';
 import Division from './division';
 
-export { Addition, Subtraction, Multiplication, Division };
+let CalculatorApp = () => {
+    return (
+        <>
+            <h1>Calculator App</h1>
+            <Addition />
+            <Subtraction />
+            <Multiplication />
+            <Division />
+        </>
+    )
+}
+
+export default CalculatorApp;
